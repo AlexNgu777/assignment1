54bf1536c1e28a96c42625f1b868a8f808021fdb
AlexNgu777 <alexnguyen@Alexs-MacBook-Pro-4.local>
2025-01-29 12:48:08 -0500

    began working on some game logic

--- src/main/java/sheridan/ngualexa/assignment1/model/BoxGame.java
+++ src/main/java/sheridan/ngualexa/assignment1/model/BoxGame.java
@@ -1,4 +1,29 @@
 package sheridan.ngualexa.assignment1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxGame {
+
+    private List<String> boxes;
+    private String userPrize;
+
+    public BoxGame() {
+        this.boxes = new ArrayList<>();
+    }
+
+    public void randomizeBoxes() {
+        List<String> prizes = List.of("1","2","3");
+        boxes = new ArrayList<>(prizes);
+
+    }
+
+    public String selectedBox(int boxIndex) {
+        this.userPrize = boxes.get(boxIndex);
+        return userPrize;
+    }
+
+    public List<String> getBoxes() {
+        return boxes;
+    }
 }
